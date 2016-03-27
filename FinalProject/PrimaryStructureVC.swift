cb95940d95903d0f03f354525e3ad157fbec876d
Alex Pinto <alexandriapinto1@gmail.com>
2016-03-27 17:44:03 -0400

    primaryfinished atoryboard

--- FinalProject/PrimaryStructureVC.swift
+++ FinalProject/PrimaryStructureVC.swift
@@ -10,5 +10,60 @@ import Foundation
 import UIKit
 
 class PrimaryStructureVC: UIViewController{
+    @IBOutlet weak var peptidebond1: UIImageView!
+    @IBOutlet weak var peptideBond2: UIImageView!
+    @IBOutlet weak var peptideBond3: UIImageView!
+    @IBOutlet weak var peptideBond4: UIImageView!
     
+    
+    @IBOutlet weak var peptideBond5: UIImageView!
+    
+    @IBOutlet weak var peptideBond6: UIImageView!
+    
+    @IBOutlet weak var peptideBond7: UIImageView!
+    
+    @IBOutlet weak var peptideBond8: UIImageView!
+    
+    @IBOutlet weak var peptideBond9: UIImageView!
+    @IBOutlet weak var peptideBond10: UIImageView!
+  
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.peptidebond1.transform = CGAffineTransformMakeRotation(-(35.0 * CGFloat(M_PI)) / 180.0)
+        
+        self.peptideBond2.transform = CGAffineTransformMakeRotation((10.0 * CGFloat(M_PI)) / 180.0)
+        
+        self.peptideBond3.transform = CGAffineTransformMakeRotation(-(135.0 * CGFloat(M_PI)/180.0))
+        
+        self.peptideBond4.transform = CGAffineTransformMakeRotation(-( 30.0 * CGFloat(M_PI))/180.0)
+
+        self.peptideBond5.transform = CGAffineTransformMakeRotation(-(15.0 * CGFloat(M_PI)/180.0))
+        
+        
+        self.peptideBond6.transform = CGAffineTransformMakeRotation(60.0 * CGFloat(M_PI) / 180.0)
+        
+        self.peptideBond7.transform = CGAffineTransformMakeRotation(10.0 * CGFloat(M_PI) / 180.0)
+        self.peptideBond8.transform = CGAffineTransformMakeRotation(10.0 * CGFloat(M_PI) / 180.0)
+        self.peptideBond9.transform = CGAffineTransformMakeRotation(-(70.0 * CGFloat(M_PI) / 180.0))
+        self.peptideBond10.transform = CGAffineTransformMakeRotation(-(10.0 * CGFloat(M_PI) / 180.0))
+ 
+    }
+    
+    @IBAction func sendingtoCarbon(sender: AnyObject) {
+        performSegueWithIdentifier("sentCarbon", sender: self)
+        
+    }
+    
+    @IBAction func sendtoPeptideVC(sender: AnyObject) {
+        performSegueWithIdentifier("phipsiSegue", sender: self)
+    }
+    
+    @IBAction func handlePan(recognizer:UITapGestureRecognizer) {
+        print("huh")
+    }
+
 }
--- FinalProject/aminoTableVC.swift
+++ FinalProject/aminoTableVC.swift
@@ -9,35 +9,46 @@
 import Foundation
 import UIKit
 
-class aminoTableVC: UITableViewController{
-    let aminoInformationArray = [
-            aminoContentStruct(name: "Alanine", threeLetterAbrv: "Ala", singleLetterAbrv: "A", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Arginine", threeLetterAbrv: "Arg", singleLetterAbrv: "R", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Asparagine", threeLetterAbrv: "Asn", singleLetterAbrv: "N", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Aspartic Acid", threeLetterAbrv: "Asp", singleLetterAbrv: "D", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Cysteine", threeLetterAbrv: "Cys", singleLetterAbrv: "C", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Glutamine", threeLetterAbrv: "Gln", singleLetterAbrv: "Q", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Glutamic Acid", threeLetterAbrv: "Glu", singleLetterAbrv: "E", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Glycine", threeLetterAbrv: "Gly", singleLetterAbrv: "G", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Histidine", threeLetterAbrv: "His", singleLetterAbrv: "H", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Isoleucine", threeLetterAbrv: "Ile", singleLetterAbrv: "I", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Leucine", threeLetterAbrv: "Leu", singleLetterAbrv: "L", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Lysine", threeLetterAbrv: "Lys", singleLetterAbrv: "K", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Methionine", threeLetterAbrv: "Met", singleLetterAbrv: "M", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Phenylalanine", threeLetterAbrv: "Phe", singleLetterAbrv: "F", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Proline", threeLetterAbrv: "Pro", singleLetterAbrv: "P", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Serine", threeLetterAbrv: "Ser", singleLetterAbrv: "S", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Threonine", threeLetterAbrv: "Thr", singleLetterAbrv: "T", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Tryptophan", threeLetterAbrv: "Trp", singleLetterAbrv: "W", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Tyrosine", threeLetterAbrv: "Tyr", singleLetterAbrv: "Y", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-            aminoContentStruct(name: "Valine", threeLetterAbrv: "Val", singleLetterAbrv: "V", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
-        ]
+
+class aminoTableVC: UITableViewController  {
     
+    var aminoNames: [String] = ["Alanine", "Arginine", "Asparagine", "Aspartic Acid", "Cysteine", "Glutamine", "Glutamic Acid", "Glycine", "Histidine", "Isoleucine", "Leucine", "Lysine", "Methionine",  "Phenylalanine", "Proline", "Serine", "Threonine", "Tryptophan", "Tyrosine", "Valine"]
     var placeholderAmino: aminoContentStruct!
+    var aminoInformationArray: [aminoContentStruct]!
+    
+//    init(){
+//        aminoInformationArray = [
+//            aminoContentStruct(name: "Alanine", threeLetterAbrv: "Ala", singleLetterAbrv: "A", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Arginine", threeLetterAbrv: "Arg", singleLetterAbrv: "R", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Asparagine", threeLetterAbrv: "Asn", singleLetterAbrv: "N", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Aspartic Acid", threeLetterAbrv: "Asp", singleLetterAbrv: "D", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Cysteine", threeLetterAbrv: "Cys", singleLetterAbrv: "C", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Glutamine", threeLetterAbrv: "Gln", singleLetterAbrv: "Q", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Glutamic Acid", threeLetterAbrv: "Glu", singleLetterAbrv: "E", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Glycine", threeLetterAbrv: "Gly", singleLetterAbrv: "G", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Histidine", threeLetterAbrv: "His", singleLetterAbrv: "H", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Isoleucine", threeLetterAbrv: "Ile", singleLetterAbrv: "I", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Leucine", threeLetterAbrv: "Leu", singleLetterAbrv: "L", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Lysine", threeLetterAbrv: "Lys", singleLetterAbrv: "K", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Methionine", threeLetterAbrv: "Met", singleLetterAbrv: "M", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Phenylalanine", threeLetterAbrv: "Phe", singleLetterAbrv: "F", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Proline", threeLetterAbrv: "Pro", singleLetterAbrv: "P", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Serine", threeLetterAbrv: "Ser", singleLetterAbrv: "S", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Threonine", threeLetterAbrv: "Thr", singleLetterAbrv: "T", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Tryptophan", threeLetterAbrv: "Trp", singleLetterAbrv: "W", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Tyrosine", threeLetterAbrv: "Tyr", singleLetterAbrv: "Y", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//            aminoContentStruct(name: "Valine", threeLetterAbrv: "Val", singleLetterAbrv: "V", sideChain: nil, backGroundColor: nil, extendedProperties: "", property: ""),
+//        ]
+//        super.init()
+//    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
         
     }
     
@@ -48,13 +59,13 @@ class aminoTableVC: UITableViewController{
     
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return self.aminoNames.count
+        
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell")!
-        cell.textLabel!.text = aminoInformationArray[indexPath.row]
-        
-                return cell
+        cell.textLabel?.text = self.aminoNames[indexPath.row].uppercaseString
+        return cell
         
     }
     
