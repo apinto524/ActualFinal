2a630fb5c2d292e4bc0c9ccc7532dcc053f32178
Alex Pinto <alexandriapinto1@gmail.com>
2016-05-03 09:36:56 -0400

    quiz start setup

---
+++ FinalProject/carbonBackBone.swift
@@ -0,0 +1,17 @@
+//
+//  carbonBackBone.swift
+//  FinalProject
+//
+//  Created by Alexandria Pinto on 5/2/16.
+//  Copyright © 2016 Alexandria Pinto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class carbonBackBone: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(imageLiteral: "background"))
+    }
+}
--- FinalProject/quizYourself.swift
+++ FinalProject/quizYourself.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 import UIKit
-
+var count = 0
 class quizYourself: UIViewController{
     
     @IBOutlet weak var imageView: UIImageView!
@@ -21,33 +21,61 @@ class quizYourself: UIViewController{
     @IBOutlet weak var answerD: UILabel!
     @IBOutlet weak var score: UILabel!
     @IBOutlet weak var viewControllerButton: UIButton!
+    var scoreNumber = 0
     
+
     
-    let allQuestions = [quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
-        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: )]
+    var questionToBeShown: quizStruct!
+    
+    var correctQuestions: [Int] = []
+    
+    var correctButton: String = ""
+    
+    let allQuestions = [quizStruct(question: "What is the name of this peptide?", answerA: "Valine" , answerB: "Isoleucine" , answerC: "Asparagine", answerD: "Glycine", correctAnswer: "B" , image: "isoleucinePic" , findAnswerVC: "aminoTableVC"),
+        quizStruct(question: "What is the term for the arrangement of multiple peptides to from a protein?", answerA: "primary structure", answerB: "secondary structure", answerC: "tertiary structure" , answerD: "quaternary structure", correctAnswer: "D", image: nil, findAnswerVC: "TertiaryQuaternaryStructVC"),
+        quizStruct(question: "Which of the following is the term for the order of amino acids?", answerA: "primary structure" , answerB: "secondary structure" , answerC: "tertiary structure", answerD: "quaternary structure", correctAnswer: "A", image: nil, findAnswerVC: "PrimaryStructureVC"),
+        quizStruct(question: "Which of the following refers to the three dimensional structure of a single peptide chain?" , answerA: "primary structure" , answerB: "secondary structure", answerC: "tertiary structure", answerD: "quaternary structure" , correctAnswer: "D" , image: nil, findAnswerVC: "TertiaryQuaternaryStructVC" ),
+        quizStruct(question: "The primary structure is primarily maintained by:", answerA: "hydrogren bonds", answerB: "ionic bonds", answerC: "peptide bonds", answerD: "none of the above", correctAnswer: "C", image: nil, findAnswerVC: "PrimaryStructureVC"),
+        quizStruct(question: "True or false: The name of the bond between the alpha carbon and carboxyl group is the phi bond." , answerA: "True", answerB: "False", answerC: nil, answerD: nil, correctAnswer: "B", image: nil, findAnswerVC: "carbonBackBone")]
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: ),
+//        quizStruct(question: , answerA: , answerB: , answerC: , answerD: , correctAnswer: , image: , findAnswerVC: )]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(count)
         self.view.backgroundColor = UIColor(patternImage: UIImage(imageLiteral: "background"))
-    }
-    func setQuestion(){
+        setQuestion()
         
     }
-   
+    func setQuestion(){
+        if count < allQuestions.count{
+            questionToBeShown = allQuestions[count]
+            count++
+            displayInformation(questionToBeShown)
+        } else {
+            count = 0
+            setQuestion()
+        }
+    }
+    func displayInformation(questionToBeShown: quizStruct){
+        self.question.text = questionToBeShown.question
+        self.answerA.text = questionToBeShown.answerA
+        self.answerB.text = questionToBeShown.answerB
+        self.answerC.text = questionToBeShown.answerC
+        self.answerD.text = questionToBeShown.answerD
+        
+    }
+    func setCorrectAnswer(answerLetter: String){
+        self.correctButton = answerLetter
+    }
 }
